a7c017cefae6879a069473556eb522fddeba3724
NoraAyato <laithanhtrunghutech@gmail.com>
2025-08-27 23:44:19 +0700

    refactor(resources): refactor resource location and allow cache image data

--- Backend/app/src/main/java/com/bookinghomestay/app/config/WebMvcConfig.java
+++ Backend/app/src/main/java/com/bookinghomestay/app/config/WebMvcConfig.java
@@ -2,16 +2,18 @@ package com.bookinghomestay.app.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.http.CacheControl;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String avatarPath = "file:" + System.getProperty("user.dir") + "/uploads/avatars/";
-        registry.addResourceHandler("/avatars/**")
-                .addResourceLocations(avatarPath)
-                .setCachePeriod(0); // không cache để debug
-        System.out.println("Serving avatars from: " + avatarPath);
+        String imgPath = "file:" + System.getProperty("user.dir") + "/img/";
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations(imgPath)
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS).cachePublic());
+
     }
 }
