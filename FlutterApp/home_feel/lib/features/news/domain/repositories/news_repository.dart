d1559b90dcde4a79b027b4031913e13707fa9fa6
NoraAyato <laithanhtrunghutech@gmail.com>
2025-12-19 15:42:44 +0700

    Remove FlutterApp from repository

--- FlutterApp/home_feel/lib/features/news/domain/repositories/news_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:home_feel/shared/models/api_response.dart';
-import '../../data/models/news_model.dart';
-import '../../data/models/news_detail_model.dart';
-
-abstract class NewsRepository {
-  Future<ApiResponse<List<NewsModel>>> getAllNews();
-  Future<ApiResponse<NewsDetailModel>> getNewsDetail(String maTinTuc);
-}
